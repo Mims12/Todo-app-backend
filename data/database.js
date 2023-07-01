048d90dc88bd6db3c2ce765e030bafb285056302
Mims12 <lizaelzeblith@gmail.com>
2023-07-01 18:05:01 +0530

    fixed engine

--- data/database.js
+++ data/database.js
@@ -3,6 +3,6 @@ import mongoose from 'mongoose';
 export const connectDB = () =>{
 mongoose.connect(process.env.MONGO_URI,{
     dbName:"BackendAPI"
- }).then((c)=> console.log("database connectd"))
+ }).then((c)=> console.log(`database connectd with ${c.connection.host}`))
  .catch((e)=> console.log(e));
 };
